73e1e95f41cbea56370c34d33082f69e83d877bc
hyfree <wangxianqiangcn@foxmail.com>
2024-05-30 23:10:47 +0800

    upadte

--- ScoreCalculator/Views/Windows/AboutWindow.xaml.cs
+++ ScoreCalculator/Views/Windows/AboutWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace ScoreCalculator.Views.Windows
                 商用密码应用安全性测评计算工具
                 MPScoreCalculator V0.1
                 使用MPScoreCalculator计算分数和洞察系统风险 ！
-                开源地址:https://github.com/hyfree/MPScoreCalculator
+                开源地址: https://github.com/hyfree/MPCal
                 开源协议：GPLv3协议
                 问题反馈：https://github.com/hyfree/MPScoreCalculator/issues
                 邮箱：wangxianqiangcn@foxmail.com，来信标题请注明MPScoreCalculator
